8958daf68d4c287ae85d8d03c6e34f9980a9424e
unknown <masterwlaster@gmail.com>
2022-06-12 14:59:30 +0300

    new

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module goognion
 
 go 1.18
+
+require (
+	github.com/cretz/bine v0.2.0
+	golang.org/x/net v0.0.0-20220607020251-c690dde0001d
+)
+
+require (
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
--- src/repository/TorCrawlerRepository.go
+++ src/repository/TorCrawlerRepository.go
@@ -1,7 +1,14 @@
 package repository
 
 import (
+	"bytes"
+	"context"
+	"github.com/cretz/bine/tor"
+	"golang.org/x/net/html"
 	"goognion/src"
+	"net/http"
+	"strings"
+	"time"
 )
 
 type TorCrawlerRepository struct {
@@ -11,12 +18,57 @@ func NewTorCrawlerRepository() *TorCrawlerRepository {
 	return &TorCrawlerRepository{}
 }
 
-func (t *TorCrawlerRepository) Load(url string) ([]src.Text, []string, error) {
+func (r *TorCrawlerRepository) Load(url string) ([]src.Text, []string, error) {
+	t, err := tor.Start(nil, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer t.Close()
+
+	// Wait at most a minute to start network and get
+	dialCtx, dialCancel := context.WithTimeout(context.Background(), time.Minute)
+	defer dialCancel()
+
+	// Make connection
+	dialer, err := t.Dialer(dialCtx, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	httpClient := &http.Client{Transport: &http.Transport{DialContext: dialer.DialContext}}
+
+	// Get /
+	resp, err := httpClient.Get(url)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer resp.Body.Close()
+
+	// Grab the <title>
+	_, err = html.Parse(resp.Body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return nil, nil, err
+}
+
+func (r *TorCrawlerRepository) DoIndexing(src []src.Text) (map[string]int, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (t *TorCrawlerRepository) DoIndexing(src []src.Text) (map[string]int, error) {
-	//TODO implement me
-	panic("implement me")
+func getTitle(n *html.Node) string {
+	if n.Type == html.ElementNode && n.Data == "title" {
+		var title bytes.Buffer
+		if err := html.Render(&title, n.FirstChild); err != nil {
+			panic(err)
+		}
+		return strings.TrimSpace(title.String())
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if title := getTitle(c); title != "" {
+			return title
+		}
+	}
+	return ""
 }
